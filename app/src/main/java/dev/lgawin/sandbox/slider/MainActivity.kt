cd6b6b769e516085686bba16fe10c431387b9711
Łukasz Gawin <lukasz.gawin@foqus.mobi>
2025-10-14 22:04:33 +0200

    Cleanups and adjustments

--- app/src/main/java/dev/lgawin/sandbox/slider/MainActivity.kt
+++ app/src/main/java/dev/lgawin/sandbox/slider/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.lgawin.sandbox.slider.ui.theme.SliderSandboxTheme
+import dev.lgawin.sandbox.slider.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SliderSandboxTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SliderSandboxTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
