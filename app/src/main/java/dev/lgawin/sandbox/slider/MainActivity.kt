f86bbd8ba87c939eb2b09ae0da34881a685e9b1e
Łukasz Gawin <lukasz.gawin@foqus.mobi>
2025-10-16 04:52:15 +0200

    show direct input dialog

--- app/src/main/java/dev/lgawin/sandbox/slider/MainActivity.kt
+++ app/src/main/java/dev/lgawin/sandbox/slider/MainActivity.kt
@@ -6,22 +6,45 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.SaverScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
@@ -75,6 +98,90 @@ class MainActivity : ComponentActivity() {
                                 viewModel.update(SliderValue.Settled(sliderValue))
                             },
                         )
+                        var showDialog by rememberSaveable { mutableStateOf(false) }
+                        AnimatedContent(
+                            showDialog,
+                            transitionSpec = {
+                                fadeIn(
+                                    animationSpec = tween(durationMillis = 300, delayMillis = 300)
+                                ) togetherWith fadeOut(animationSpec = tween(300))
+                            },
+                        ) { isDialogShown ->
+                            if (isDialogShown) {
+                                DirectInputDialog(
+                                    onSubmit = { viewModel.update(SliderValue.Settled(it.toFloat())) },
+                                    onDismissRequest = { -> showDialog = false })
+                            } else {
+                                Button(
+                                    onClick = { showDialog = !showDialog },
+                                    modifier = Modifier.align(Alignment.End),
+                                ) {
+                                    Text("Direct Input")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DirectInputDialog(
+    onSubmit: (Int) -> Unit,
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(300.dp)
+                .padding(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 32.dp),
+            ) {
+                var input by remember { mutableStateOf("") }
+                val focusRequester = remember { FocusRequester() }
+                TextField(
+                    value = input,
+                    onValueChange = { input = it },
+                    modifier = Modifier.focusRequester(focusRequester),
+                    label = { Text("Input integer value") },
+                )
+                LaunchedEffect(Unit) {
+                    focusRequester.requestFocus()
+                }
+                val parsedToInt by remember { derivedStateOf { input.toIntOrNull() } }
+                Spacer(
+                    modifier = Modifier
+                        .heightIn(min = 16.dp)
+                        .weight(1f),
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Cancel")
+                    }
+                    TextButton(
+                        onClick = {
+                            parsedToInt?.let { onSubmit(it) }
+                            input = ""
+                            onDismissRequest()
+                        },
+                        modifier = Modifier.padding(8.dp),
+                        enabled = parsedToInt != null,
+                    ) {
+                        Text("Submit")
                     }
                 }
             }
@@ -90,9 +197,7 @@ class SliderViewModel(
 
     init {
         viewModelScope.launch {
-            value.map { it.roundToInt() }
-                .distinctUntilChanged()
-                .collect(controller::setValue)
+            value.map { it.roundToInt() }.distinctUntilChanged().collect(controller::setValue)
         }
     }
 
